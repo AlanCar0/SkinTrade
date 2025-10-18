43dc43084c470b0dad463cd5fd4ba98a790117d9
Alansito-kun <al.carov@duocuc.cl>
2025-10-17 23:25:41 -0300

    Test
    Probando que la vaina funcione

--- SkinTrade/app/src/main/java/com/example/skintrade/MainActivity.kt
+++ SkinTrade/app/src/main/java/com/example/skintrade/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SkinTradeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Mundo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SkinTradeTheme {
-        Greeting("Android")
+        Greeting("Mundo")
     }
 }
